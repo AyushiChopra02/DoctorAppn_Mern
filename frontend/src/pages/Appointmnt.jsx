b2fa75186e6dfb65397400d995bbe750186dd289
AyushiChopra02 <ayushichopra.02@gmail.com>
2025-03-10 02:46:48 +0530

    add

---
+++ frontend/src/pages/Appointmnt.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Appointmnt = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Appointmnt
---
+++ frontend/src/pages/MyAppnt.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyAppnt = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyAppnt
