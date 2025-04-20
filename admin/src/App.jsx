8254f527c995c7d5f4bab843a5bb424d042493f5
AyushiChopra02 <ayushichopra.02@gmail.com>
2025-04-20 12:58:35 +0530

    add

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,24 +1,32 @@
 import React, { useContext } from 'react';
 import Login from './pages/Login';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer , toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { AppContext } from './context/AppContext'; // ✅ FIXED
+import { AdminContext } from './context/AdminContext'; 
 import Navbar from './components/Navbar';
 
 const App = () => {
-  const { aToken } = useContext(AppContext);
+  const { aToken } = useContext(AdminContext);
 
   return aToken ? (
     <div className='bg-[#F8F9FD]'>
       <ToastContainer />
       <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={< />} />
+          <Route path='/' element={<></>} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
       <Login />
       <ToastContainer />
     </>
-  );
-};
+  )
+}
 
 export default App;
